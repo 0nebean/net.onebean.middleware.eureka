9ca4eb509dfc9f8a69077aecfdba1c924866ee7f
0neBean <pein790751571@aliyun.com>
2020-07-04 12:56:39 +0800

    no message

--- src/main/java/net/onebean/spring/Main.java
+++ src/main/java/net/onebean/spring/Main.java
@@ -1,13 +1,11 @@
 package net.onebean.spring;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import net.onebean.core.extend.ApolloConfInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableApolloConfig
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class Main  {
